268c44e6aa431851a2ec58161659c8814ec3fcb4
leandrospan <leandrospan@gmail.com>
2023-09-25 11:03:03 -0300

    Início da tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,18 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'filter',
     loadChildren: () => import('./filter/filter.module').then( m => m.FilterPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  login = {
+    email: "",
+    password: "",
+  };
+
+  type : boolean = true;
+
+  constructor() { }
+
+  changeType(){
+    this.type = !this.type;
+  }
+
+  gotoHome(){
+
+  }
+
+  gmailLogin(){
+
+  }
+
+  twitterLogin(){
+
+  }
+
+  facebookLogin(){
+
+  }
+
+  touchLogin(){
+    
+  }
+
+  gotToForget(){
+
+  }
+
+  ngOnInit() {
+  }
+
+}

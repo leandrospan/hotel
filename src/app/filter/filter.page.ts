f368b8a34723fc98ecd74dc6b20048ff881c97de
leandrospan <leandrospan@gmai.com>
2023-09-25 06:09:02 -0300

    Término da tela de filtros

--- src/app/filter/filter.page.ts
+++ src/app/filter/filter.page.ts
@@ -12,10 +12,10 @@ export class FilterPage implements OnInit {
     "cozinha" : "indiana",
     "distancia" : 5,
     "popular" : true,
-    "ordemdepreco" : 'altobaixo',
+    "ordemdepreco" : "altobaixo",
     "preco" : {
-      "precoBaixo" : "5",
-      "precoAlto" : "10",
+      "lower" : "5",
+      "upper" : "10",
     }
   }
 
@@ -25,6 +25,57 @@ export class FilterPage implements OnInit {
     console.log(ev.detail.value);
     const tipo = ev.detail.value;
     this.filtros.tipo_carne = tipo;
+  }
+
+  tipoCozinhaChange(ev : any){
+    console.log(ev.detail.value);
+    const tipo = ev.detail.value;
+    this.filtros.cozinha = tipo;
+  }
+
+  tipoDistanciaChange(ev : any){
+    console.log(ev.detail.value);
+    const tipo = ev.detail.value;
+    this.filtros.distancia = tipo;
+  }
+
+  tipoPopularChange(ev : any){
+    console.log(ev.detail.value);
+    const tipo = ev.detail.value;
+    this.filtros.popular = tipo;
+  }
+
+  tipoOrdemPrecoChange(ev : any){
+    console.log(ev.detail.value);
+    const tipo = ev.detail.value;
+    this.filtros.ordemdepreco = tipo;
+  }
+
+  tipoPrecoChange(ev : any){
+    console.log(ev.detail.value);
+    const tipo = ev.detail.value;
+    this.filtros.preco.lower = tipo.lower;
+    this.filtros.preco.upper = tipo.upper;
+    console.log(this.filtros.preco.lower);
+    console.log(this.filtros.preco.upper);
+  }
+
+  reset(){
+    this.filtros = {
+      "tipo_carne" : "almoco",
+      "cozinha" : "indiana",
+      "distancia" : 5,
+      "popular" : true,
+      "ordemdepreco" : "altobaixo",
+      "preco" : {
+        "lower" : "5",
+        "upper" : "10",
+      }
+    }
+  }
+
+  aplicarFiltros(){
+    console.log(this.filtros);
   }
 
   ngOnInit() {

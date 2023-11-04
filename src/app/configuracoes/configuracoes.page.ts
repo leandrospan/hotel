e9f05453027e1fd7f0004eaa6c92d1dbcf8417ee
leandrospan <leandrospan@gmai.com>
2023-11-04 11:39:38 -0300

    Término da tela de configuração

--- src/app/configuracoes/configuracoes.page.ts
+++ src/app/configuracoes/configuracoes.page.ts
@@ -7,7 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfiguracoesPage implements OnInit {
 
+  setting: any = {
+    appSettings: true,
+    sms: true,
+    offer: true,
+    reminder: '',
+    touchId: true,
+    faceId: true,
+  };
+
   constructor() { }
+
+  public changeApp(ev: any){
+    this.setting.appSettings = ev.detail.checked;
+    console.log(this.setting);
+  }
+
+  public changeSms(ev: any){
+    this.setting.sms = ev.detail.checked;
+    console.log(this.setting);
+  }
+
+  public changeOffer(ev: any){
+    this.setting.offer = ev.detail.checked;
+    console.log(this.setting);
+  }
+
+  public changeReminder(ev: any){
+    this.setting.reminder = ev.detail.checked;
+    console.log(this.setting);
+  }
+
+  public changeTouchId(ev: any){
+    this.setting.touchId = ev.detail.checked;
+    console.log(this.setting);
+  }
+
+  public changeFaceId(ev: any){
+    this.setting.faceId = ev.detail.checked;
+    console.log(this.setting);
+  }
 
   ngOnInit() {
   }
